981c517c0668678ad12039fce332bf3e317065d7
Artur <arktur.usmanov@yandex.com>
2019-02-25 11:06:33 +0300

    npm run build config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/spa/dist/',
+    publicPath: '/spa/',
     filename: 'build.js'
   },
   module: {
